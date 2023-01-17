9383d621065e6d930ae9fa216b4877033c447813
mdallen5393 <allen_matthew@outlook.com>
2023-01-17 13:08:03 -0600

    Task 1

---
+++ 0x04-TypeScript/task_0/js/main.js
@@ -0,0 +1,13 @@
+var student1 = {
+    firstName: "fn1",
+    lastName: "ln2",
+    age: 23,
+    location: "here"
+};
+var student2 = {
+    firstName: "fn2",
+    lastName: "ln2",
+    age: 23,
+    location: "there"
+};
+var studentsList = [student1, student2];
