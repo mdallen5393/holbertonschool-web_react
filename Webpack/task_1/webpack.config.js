393b4d3e9f63bd381d1db417311708e4a3b966e8
mdallen5393 <allen_matthew@outlook.com>
2023-09-06 14:18:47 -0500

    Task 1 initial attempt

---
+++ Webpack/task_1/webpack.config.js
@@ -0,0 +1,11 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production'
+}
