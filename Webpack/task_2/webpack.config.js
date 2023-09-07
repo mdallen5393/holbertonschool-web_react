02d46c89e7b71be3e0cd431efd0202107f8c2413
mdallen5393 <allen_matthew@outlook.com>
2023-09-07 10:21:34 -0500

    Task 2 first attempt

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      }
+    ]
+  }
+}
