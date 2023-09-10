c15027208eb74471f75c36f8b429681e44463700
mdallen5393 <allen_matthew@outlook.com>
2023-09-10 16:19:49 -0500

    Many unsuccessful tests to get testing to work

---
+++ react_intro/task_3/dashboard/src/App.test.js
@@ -0,0 +1,53 @@
+// OMG LITERALLY NOTHING WORKS
+
+// USING REACT TESTING LIBRARY DUE TO ENZYME INCOMPATIBILITY WITH REACT 18
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('App component renders', () => {
+//   render(<App />);
+//   const headerDiv = screen.getByTestId('App-header');
+//   expect(headerDiv).toBeInTheDocument();
+// });
+
+// USING ENZYME
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+describe('App Component', () => {
+  let wrapper;
+
+  // Re-creates wrapper before each test to prevent side-effects or
+  // interference between tests
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  // Test that App renders without crashing
+  it('renders without crashing', () => {
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  // Test that App renders a div with the class App-header
+  it('renders a div with class App-header', () => {
+    wrapper.update();
+    const headerDiv = wrapper.find('.App-header');
+    expect(headerDiv.exists()).toBe(true);
+  });
+
+  // Test that App renders a div with the class App-body
+  it('renders a div with class App-body', () => {
+    wrapper.update();
+    const bodyDiv = wrapper.find('.App-body');
+    expect(bodyDiv.exists()).toBe(true);
+  });
+
+  // Test that App renders a div with the class App-footer
+  it('renders a div with class App-footer', () => {
+    wrapper.update();
+    const footerDiv = wrapper.find('.App-footer');
+    expect(footerDiv.exists()).toBe(true);
+  });
+});
--- react_intro/task_4/dashboard/src/setupTests.js
+++ react_intro/task_4/dashboard/src/setupTests.js
@@ -5,8 +5,7 @@
 import '@testing-library/jest-dom';
 
 import Enzyme from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from 'enzyme-adapter-react-16';
 
 // Configure Enzyme with the adapter
 Enzyme.configure({ adapter: new Adapter() });
