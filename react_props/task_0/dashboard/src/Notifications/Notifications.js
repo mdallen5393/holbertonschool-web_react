15438c520b0b2f18a85e11e5473b24a29657619b
mdallen5393 <allen_matthew@outlook.com>
2023-09-11 18:31:35 -0500

    Completed up to and including task 3; created files for task 4

--- react_props/task_0/dashboard/src/Notifications/Notifications.js
+++ react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Notifications.css';
 import closeIcon from '../assets/close-icon.png';
 import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
 
 export default function Notifications() {
   const handleButtonClick = () => {
@@ -18,9 +19,9 @@ export default function Notifications() {
 
       <p>Here is the list of notifications</p>
       <ul>
-        <li data='default'>New course available</li>
-        <li data='urgent'>New resume available</li>
-        <li data='urgent' dangerouslySetInnerHTML={{__html: getLatestNotification()}}></li>
+        <NotificationItem type='default' value='New course available' />
+        <NotificationItem type='urgent' value='New resume available' />
+        <NotificationItem type='urgent' html={{ __html: getLatestNotification() }} />
       </ul>
     </div>
   </>
--- react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -17,16 +17,22 @@ describe('Notifications Component', () => {
   });
 
   // Test that Notifications renders three list items
-  it('renders a div with class App-header', () => {
+  it('renders three list items', () => {
     wrapper.update();
-    const listItems = wrapper.find('li');
+    const listItems = wrapper.find('NotificationItem');
     expect(listItems.length).toBe(3);
   });
 
   // Test that Notifications renders the correct text
-  it('renders a div with class App-body', () => {
+  it('renders the correct text', () => {
     wrapper.update();
     const p = wrapper.find('p');
     expect(p.text()).toEqual('Here is the list of notifications');
+  });
+
+  // Test that Notifications renders the correct NotificationItem
+  it('renders the first NotificationItem html correctly', () => {
+    wrapper.update();
+    expect(wrapper.html()).toContain('<div class=\"Notifications\"><img src=\"test-file-stub\" alt=\"close icon\" style=\"height: 15px; position: absolute; top: 10px; right: 10px;\" aria-label=\"Close\"><p>Here is the list of notifications</p><ul><li data-notification-type=\"default\">New course available</li><li data-notification-type=\"urgent\">New resume available</li><li data-notification-type=\"urgent\"><strong>Urgent requirement</strong> - complete by EOD</li></ul></div>');
   });
 });
