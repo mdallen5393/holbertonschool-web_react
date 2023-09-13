130ae3164ee1c868b265e9b373d2eb2c852e6a44
mdallen5393 <allen_matthew@outlook.com>
2023-09-13 13:19:17 -0500

    Task 0. added inline styling for rows

--- React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const rowStyle = {
+  backgroundColor: '#f5f5f5ab',
+};
+
+const headerRowStyle = {
+  backgroundColor: '#deb5b545',
+};
+
 const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
   if (isHeader) {
     if (textSecondCell === null) {
       return (
-        <tr>
-          <th colSpan="2">{textFirstCell}</th>
+        <tr >
+          <th colSpan="2" style={isHeader ? headerRowStyle : rowStyle}>{textFirstCell}</th>
         </tr>
       );
     }
     return (
       <tr className='column-headers'>
-        <th>{textFirstCell}</th>
-        <th>{textSecondCell}</th>
+        <th style={isHeader ? headerRowStyle : rowStyle}>{textFirstCell}</th>
+        <th style={isHeader ? headerRowStyle : rowStyle}>{textSecondCell}</th>
       </tr>
     );
   }
   return (
-    <tr>
+    <tr style={rowStyle}>
       <td>{textFirstCell}</td>
       <td>{textSecondCell}</td>
     </tr>
