2ce8f9dfe3cf240c83c32c816a2ec500ff8fa1c7
mdallen5393 <allen_matthew@outlook.com>
2023-09-13 14:31:14 -0500

    copied files from task 0

--- React_inline_styling/task_0/task_5/dashboard/config/setupTests.js
+++
@@ -1,6 +0,0 @@
-require('jsdom-global')();
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-// Configure Enzyme with the adapter
-Enzyme.configure({ adapter: new Adapter() });
