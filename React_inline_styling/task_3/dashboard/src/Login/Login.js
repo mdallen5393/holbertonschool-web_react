f497d8f515b39fcfb26a8f3f505670b87b33a199
mdallen5393 <allen_matthew@outlook.com>
2023-09-14 11:26:33 -0500

    Made Login component responsive

--- React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -7,12 +7,16 @@ const Login = () => (
     <BodySectionWithMarginBottom title='Log in to continue'>
       <div className={`Login ${css(styles.login)}`}>
         <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor='email' >Email</label>
-          <input type='email' name='email' id='email' autoComplete='email'></input>
-          <label htmlFor='password'>Password</label>
-          <input type='password' name='password' id='password' autoComplete='true'></input>
-          <button>OK</button>
+        <form className={css(styles.form)}>
+          <div className={css(styles.labelInput)}>
+            <label htmlFor='email' >Email</label>
+            <input type='email' name='email' id='email' autoComplete='email'></input>
+          </div>
+          <div className={css(styles.labelInput)}>
+            <label htmlFor='password'>Password</label>
+            <input type='password' name='password' id='password' autoComplete='true'></input>
+          </div>
+          <button className={css(styles.button)}>OK</button>
         </form>
       </div>
     </BodySectionWithMarginBottom>
@@ -22,6 +26,19 @@ const Login = () => (
 const styles = StyleSheet.create({
   login: {
     margin: '30px 30px auto',
+  },
+  form: {
+    display: 'flex',
+    '@media screen and (max-width: 900px)': {
+      flexDirection: 'column',
+    },
+  },
+  button: {
+    maxWidth: 40,
+  },
+  labelInput: {
+    display: 'flex',
+    flexDirection: 'row',
   },
 });
 
